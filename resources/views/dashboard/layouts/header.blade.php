0e6b75c922eadb5a0ca8880ceb6affe507395093
rendy oktavian <rendyokts08@gmail.com>
2024-05-06 08:37:20 +0700

    Portal Berita

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -17,4 +17,4 @@
     <div id="navbarSearch" class="navbar-search w-100 collapse">
       <input class="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
     </div>
-  </header>
+</header>
